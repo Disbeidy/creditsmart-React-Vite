d0bee2dc64369821add3b42608fba8fb1a5035f3
Disbeidy Anzueta Gongora <disbeidyanzueta@gmail.com>
2025-12-04 22:21:36 -0500

    Cambios en el Footer.jsx

---
+++ creditsmart/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container footer-container">
+        <div className="footer-col">
+          <h5>CreditSmart</h5>
+          <p>Tu aliado confiable para encontrar el crédito ideal.</p>
+        </div>
+        <div className="footer-col">
+          <h5>Enlaces útiles</h5>
+          <a href="#">Política de privacidad</a>
+        </div>
+        <div className="footer-col">
+          <h5>Contáctanos</h5>
+          <p>📍 Villanueva, Colombia</p>
+          <p>📞 +57 300 452 7597</p>
+          <p>📧 contacto@creditsmart.co</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
